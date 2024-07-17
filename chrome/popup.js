6c504f95bfabf28dd19980b7b2f7d7e43ddb812b
Sicarius <amurat654@gmail.com>
2024-07-17 21:06:17 +0200

    Update default file suffix for js file types

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // Load the stored URL and base path when the popup is opened
-  chrome.storage.sync.get(['jsUrl', 'basePath'], (data) => {
+  // Load the stored URL, base path, and file extension when the popup is opened
+  chrome.storage.sync.get(['jsUrl', 'basePath', 'fileExtension'], (data) => {
     if (data.jsUrl) {
       document.getElementById('jsUrl').value = data.jsUrl;
       updateBasePath(data.jsUrl);
+      updateFileExtension(data.jsUrl);
     }
     if (data.basePath) {
       document.getElementById('basePath').value = data.basePath;
@@ -16,6 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('jsUrl').addEventListener('input', () => {
     const jsUrl = document.getElementById('jsUrl').value;
     updateBasePath(jsUrl);
+    updateFileExtension(jsUrl);
   });
 
   document.getElementById('loadChunks').addEventListener('click', () => {
@@ -24,8 +26,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const fileExtension = document.getElementById('fileExtension').value;
 
     if (jsUrl && basePath) {
-      // Save the URL and base path to chrome.storage.sync
-      chrome.storage.sync.set({ jsUrl, basePath });
+      // Save the URL, base path, and file extension to chrome.storage.sync
+      chrome.storage.sync.set({ jsUrl, basePath, fileExtension });
 
       chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         chrome.tabs.executeScript(tabs[0].id, {
@@ -39,4 +41,14 @@ document.addEventListener('DOMContentLoaded', () => {
 function updateBasePath(url) {
   const basePath = url.substring(0, url.lastIndexOf('/') + 1);
   document.getElementById('basePath').value = basePath;
+}
+
+function updateFileExtension(url) {
+  const extensionInput = document.getElementById('fileExtension');
+  const mainJsPattern = /main\.\w+\.js$/;
+  if (mainJsPattern.test(url)) {
+    extensionInput.value = '.chunk.js';
+  } else {
+    extensionInput.value = '.js';
+  }
 }
